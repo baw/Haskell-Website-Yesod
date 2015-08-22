c3b6381ee4d9bb6e3c103ddfc5b588c7c0e361c1
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2015-08-22 10:41:52 -0700

    add initial projects link

---
+++ Handler/Projects.hs
@@ -0,0 +1,6 @@
+module Handler.Projects where
+
+import Import
+
+getProjectsR :: Handler Html
+getProjectsR = error "Not yet implemented: getProjectsR"
---
+++ test/Handler/ProjectsSpec.hs
@@ -0,0 +1,10 @@
+module Handler.ProjectsSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getProjectsR" $ do
+        error "Spec not implemented: getProjectsR"
+
