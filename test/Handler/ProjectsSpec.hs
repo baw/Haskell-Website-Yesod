1a1edbdbbaace83c75d652ce498ae3183960d07d
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2015-08-22 14:07:25 -0700

    add projects page tests

--- test/Handler/ProjectsSpec.hs
+++ test/Handler/ProjectsSpec.hs
@@ -4,7 +4,18 @@ import TestImport
 
 spec :: Spec
 spec = withApp $ do
-
     describe "getProjectsR" $ do
-        error "Spec not implemented: getProjectsR"
+        it "returns a 200 status" $ do
+          get ProjectsR
+          statusIs 200
 
+        it "has the right title" $ do
+          get ProjectsR
+          htmlAnyContain "h3.title" "Projects"
+
+        it "has projects on it" $ do
+          htmlAnyContain "a" "GitHub Repo"
+
+        it "has suggestion for checking back" $ do
+          get ProjectsR
+          htmlAnyContain "p.moreProjects" "Check back for more projects!"
