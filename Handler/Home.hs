1cc46e135be92df61215e656d956893a8cc0a796
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2015-08-22 23:00:41 -0700

    add projects/

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,10 +1,11 @@
 module Handler.Home where
 
 import Import
+import Text.Julius (rawJS)
 
 getHomeR :: Handler Html
 getHomeR = do
-    let handlerName = "getHomeR" :: Text
+    let clickId = "" :: Text
     defaultLayout $ do
-        setTitle "Welcome To Yesod!"
+        setTitle "Brian Weiser - Web Developer"
         $(widgetFile "homepage")
--- Handler/Projects.hs
+++ Handler/Projects.hs
@@ -1,6 +1,11 @@
 module Handler.Projects where
 
 import Import
+import Text.Julius (rawJS)
 
 getProjectsR :: Handler Html
-getProjectsR = error "Not yet implemented: getProjectsR"
+getProjectsR = do
+    let clickId = "projectsLink" :: Text
+    defaultLayout $ do
+        setTitle "Brian Weiser - Web Developer"
+        $(widgetFile "homepage")
