bc64d076876c7f69ce9af36fbaf0687985b5cbc7
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2015-10-07 09:42:51 -0700

    add blog post show handler

--- Handler/Blog.hs
+++ Handler/Blog.hs
@@ -7,3 +7,9 @@ getBlogIndexR = do
   allPosts <- runDB $ selectList [] [Desc BlogPostId]
   defaultLayout $ do
     $(widgetFile "blog/index")
+
+getBlogShowR :: BlogPostId -> Handler Html
+getBlogShowR blogPostId = do
+  blogPost <- runDB $ get404 blogPostId
+  defaultLayout $ do
+    $(widgetFile "blog/post")
