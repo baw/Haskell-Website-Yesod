b8f0c0e214567a38b1640ab5b648495b7e0abad4
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2015-11-01 01:54:45 -0700

    fix redirects

--- Handler/Redirects.hs
+++ Handler/Redirects.hs
@@ -1,14 +1,14 @@
 module Handler.Redirects where
 
 import Import
-import qualified Data.Text as T
 
+redirect301 :: String -> Handler Html
 redirect301 = redirectWith status301
 
 getAsteroidsR :: Handler Html
 getAsteroidsR = do
-    redirect301 ("https://baw.github.io/asteroids/" :: T.Text)
+    redirect301 "https://baw.github.io/asteroids/"
 
 getCommunityChatR :: Handler Html
 getCommunityChatR = do
-    redirect301 ("http://baw.github.io/community_chat/" :: T.Text)
+    redirect301 "http://baw.github.io/community_chat/"
