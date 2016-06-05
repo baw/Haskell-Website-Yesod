00d4024af569d357c64b4acf56b42da6446b3fc8
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2016-06-05 14:30:11 -0700

    add blog tests

--- test/Handler/BlogSpec.hs
+++ test/Handler/BlogSpec.hs
@@ -8,3 +8,18 @@ spec = withApp $ do
       it "returns a 200 status" $ do
         get BlogIndexR
         statusIs 200
+
+      describe "header" $ do
+        it "has the correct title" $ do
+          get BlogIndexR
+          htmlAnyContain ".header .titleText" "Brian Weiser"
+          htmlAnyContain ".header .titleText" "Blog"
+
+        it "has an image" $ do
+          get BlogIndexR
+          htmlAnyContain ".header img" ""
+
+      describe "recent posts" $ do
+        it "has the correct title" $ do
+          get BlogIndexR
+          htmlAnyContain ".recent-posts h2" "Recent Posts"
