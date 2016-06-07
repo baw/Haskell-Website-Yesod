8a61395b2bce5183e56ca56e5e9835f25278ac00
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2016-06-06 20:49:32 -0700

    move project info to the database

--- Handler/Projects.hs
+++ Handler/Projects.hs
@@ -3,9 +3,12 @@ module Handler.Projects where
 import Import
 import Text.Julius (rawJS)
 
+import Handler.Util (getProjects)
+
 getProjectsR :: Handler Html
 getProjectsR = do
     let clickId = "projectsLink" :: Text
+    projects <- getProjects
     defaultLayout $ do
         setTitle "Brian Weiser - Web Developer"
         $(widgetFile "homepage")
--- Handler/Resume.hs
+++ Handler/Resume.hs
@@ -3,9 +3,12 @@ module Handler.Resume where
 import Import
 import Text.Julius (rawJS)
 
+import Handler.Util (getProjects)
+
 getResumeR :: Handler Html
 getResumeR = do
     let clickId = "resumeLink" :: Text
+    projects <- getProjects
     defaultLayout $ do
         setTitle "Brian Weiser - Web Developer"
         $(widgetFile "homepage")
