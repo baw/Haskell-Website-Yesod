477e003a7b644be5da4d12aeed884c53a8fe5844
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2016-06-09 20:35:09 -0700

    load page to test

--- intern-test/functional/projects.js
+++ intern-test/functional/projects.js
@@ -4,6 +4,11 @@ define([
     './util'
 ], function (tdd, assert, util) {
     tdd.suite('projectPage', function () {
+        tdd.beforeEach(function () {
+            return this.remote
+                .get('http://localhost:3000/projects');
+        });
+
         tdd.test('page lowers', function () {
             return this.remote
                 .then(util.checkPagesLowered(1, 'projects'));
--- intern-test/functional/resume.js
+++ intern-test/functional/resume.js
@@ -4,6 +4,11 @@ define([
     './util'
 ], function (tdd, assert, util) {
     tdd.suite('resumePage', function () {
+        tdd.beforeEach(function () {
+            return this.remote
+                .get('http://localhost:3000/resume');
+        });
+
         tdd.test('page lowers', function () {
             return this.remote
                 .then(util.checkPagesLowered(1, 'resume'));
