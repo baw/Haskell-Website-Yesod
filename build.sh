cef10dde5b3d5a42ccbcb7d00b0ae41329d540f3
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2016-06-10 20:59:00 -0700

    create build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+vagrant ssh -c "cd /vagrant; stack build"
+cp .stack-work/install/x86_64-linux/lts-3.1/7.10.2/bin/website ./website
+branch="deploy-$(date +%F)"
+git checkout $branch || git checkout -b $branch
+git add ./website
+git commit -m "deploy"
+
+echo "Should the branch be deployed?"
+read deploy
+
+if ([ "$deploy" == "y" ] || [ "$deploy" == "Y" ]); then
+	git push -f heroku $branch:master
+else
+	echo "not deploying"
+fi
