fb74f38dc7d43971960aac0b419e91bc495bacaa
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2021-12-24 06:42:00 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 npm run compile-less
 
 vagrant ssh -c "cd /vagrant; stack clean; stack build"
-cp .stack-work/install/x86_64-linux/lts-3.1/7.10.2/bin/website ./website
+cp /home/vagrant/.local/bin/website ./website
 branch="deploy-$(date +%F)"
 git checkout $branch || git checkout -b $branch
 git add ./website
